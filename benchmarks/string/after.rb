4f814518d86a000f900ab864998e56678ad54256
Jonathan Hefner <jonathan.hefner@gmail.com>
2016-07-08 13:10:32 -0500

    Improve String#after performance

--- benchmarks/string/after.rb
+++ benchmarks/string/after.rb
@@ -2,10 +2,26 @@ require 'benchmark/inputs'
 require_relative '../../lib/casual_support'
 
 
+class String
+
+  def after_alt1(delim)
+    i = self.index(delim)
+    i ? self.drop(i + delim.length) : ''
+  end
+
+  def after_alt2(delim)
+    self.drop((self.index(delim) || self.length) + delim.length)
+  end
+
+end
+
+
 STRING = 'abcnoonxyz'
 
 Benchmark.inputs(STRING.chars << '_') do |job|
   job.report('String#after'){|delim| STRING.after(delim) }
+  job.report('String#after_alt1'){|delim| STRING.after_alt1(delim) }
+  job.report('String#after_alt2'){|delim| STRING.after_alt2(delim) }
   job.report('String#split'){|delim| STRING.split(delim, 2)[1] || '' }
   job.compare!
 end
--- lib/casual_support/string/after.rb
+++ lib/casual_support/string/after.rb
@@ -9,7 +9,7 @@ class String
   # @return [String] portion of the string after the first +delim+
   def after(delim)
     i = self.index(delim)
-    i ? self.drop(i + delim.length) : ''
+    i ? self[i + delim.length, self.length] : ''
   end
 
 end
