ec91d7d5a06496ab1ed27d1ee0cd75eedcec3afe
Jonathan Hefner <jonathan.hefner@gmail.com>
2016-07-09 11:52:09 -0500

    Clarify String#first semantics

--- lib/casual_support/string/first.rb
+++ lib/casual_support/string/first.rb
@@ -1,7 +1,15 @@
 class String
 
   # This replaces Active Support's +String#first+, but it returns an
-  # empty string when given a negative argument.  It is faster.
+  # empty string when given a negative +limit+ argument, whereas Active
+  # Support's +String#first+ removes +limit.abs+ characters from the
+  # end of the string.  Returning an empty string makes more sense if
+  # you interpret +first+ as "keep upto +limit+ characters."  At most, a
+  # negative +limit+ should *keep* that many characters from the end of
+  # the string, rather than *remove* that many characters (but returning
+  # an empty string is a good conservative choice).
+  #
+  # This method is also faster.
   def first(limit = 1)
     limit <= 0 ? '' : self[0, limit]
   end
