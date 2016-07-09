7e1c7d51470c78cc6e758ba03e0411bb0c109bea
Jonathan Hefner <jonathan.hefner@gmail.com>
2016-07-09 16:33:00 -0500

    Fix String#from bug for negative argument values

--- lib/casual_support/string/from.rb
+++ lib/casual_support/string/from.rb
@@ -2,7 +2,6 @@ class String
 
   # This replaces Active Support's +String#from+.  It is faster.
   def from(position)
-    position += self.length if position < 0
     self[position, length]
   end
 
--- test/string/from_test.rb
+++ test/string/from_test.rb
@@ -2,16 +2,38 @@ require 'test_helper'
 
 class StringFromTest < Minitest::Test
 
+  S = 'hello'
+
   def test_from_positive
-    assert_equal 'llo', 'hello'.from(2)
+    assert_equal (S[-2] + S[-1]), S.from(S.length - 2)
+  end
+
+  def test_from_end
+    assert_equal '', S.from(S.length)
+  end
+
+  def test_from_large_positive
+    assert_nil S.from(S.length + 1)
+  end
+
+  def test_from_huge_positive
+    assert_nil S.from(S.length * 10)
   end
 
   def test_from_zero
-    assert_equal 'hello', 'hello'.from(0)
+    assert_equal_not_same S, S.from(0)
   end
 
   def test_from_negative
-    assert_equal 'lo', 'hello'.from(-2)
+    assert_equal (S[-2] + S[-1]), S.from(-2)
+  end
+
+  def test_from_large_negative
+    assert_nil S.from(S.length * -1 - 1)
+  end
+
+  def test_from_huge_negative
+    assert_nil S.from(S.length * -10)
   end
 
 end
