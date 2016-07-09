e0ee4a0fee3414bb630639710da6e565827bb6a3
Jonathan Hefner <jonathan.hefner@gmail.com>
2016-07-09 17:05:32 -0500

    Fix String#to bug for negative argument values

--- lib/casual_support/string/to.rb
+++ lib/casual_support/string/to.rb
@@ -3,7 +3,7 @@ class String
   # This replaces Active Support's +String#to+.  It is faster.
   def to(position)
     position += self.length if position < 0
-    self[0, position + 1]
+    self[0, position + 1] || ''
   end
 
 end
--- test/string/to_test.rb
+++ test/string/to_test.rb
@@ -2,16 +2,42 @@ require 'test_helper'
 
 class StringToTest < Minitest::Test
 
+  S = 'hello'
+
   def test_to_positive
-    assert_equal 'hel', 'hello'.to(2)
+    assert_equal (S[0] + S[1]), S.to(1)
+  end
+
+  def test_to_end
+    assert_equal_not_same S, S.to(S.length - 1)
+  end
+
+  def test_to_large_positive
+    assert_equal_not_same S, S.to(S.length)
+  end
+
+  def test_to_huge_positive
+    assert_equal_not_same S, S.to(S.length * 10)
   end
 
   def test_to_zero
-    assert_equal 'h', 'hello'.to(0)
+    assert_equal S[0], S.to(0)
   end
 
   def test_to_negative
-    assert_equal 'hell', 'hello'.to(-2)
+    assert_equal (S[0] + S[1]), S.to(S.length * -1 + 1)
+  end
+
+  def test_to_beginning
+    assert_equal '', S.to(S.length * -1 - 1)
+  end
+
+  def test_to_large_negative
+    assert_equal '', S.to(S.length * -1 - 2)
+  end
+
+  def test_to_huge_negative
+    assert_equal '', S.to(S.length * -10)
   end
 
 end
