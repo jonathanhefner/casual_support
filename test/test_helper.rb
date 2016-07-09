2d7e246f57b6d69ebf5916932ac48d39e9b12bc0
Jonathan Hefner <jonathan.hefner@gmail.com>
2016-07-09 13:51:05 -0500

    Add assert_equal_not_same test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,12 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'casual_support'
 
 require 'minitest/autorun'
+
+class Minitest::Test
+
+  def assert_equal_not_same(expected, actual)
+    assert_equal(expected, actual)
+    refute_same(expected, actual)
+  end
+
+end
